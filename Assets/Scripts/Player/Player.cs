873205dfb5c5c52e6b6cfc188e2f66eefa286e40
Unfame1 <Unfame1@yandex.ru>
2021-12-12 17:51:14 +0300

    Added HealthDisplay

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int health;
 
+    public event UnityAction<int> HealthChanged;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(health);
+    }
+
     public void ApplyDamage(int damage)
     {
         health -= damage;
+        HealthChanged?.Invoke(health);
         
         if (health <= 0)
             Die();
